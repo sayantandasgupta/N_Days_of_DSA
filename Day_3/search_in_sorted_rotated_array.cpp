45e619690086fe958f86a9e1d00788c31cdeebbf
Sayantan <sayantan.dasgupta21@gmail.com>
2021-07-23 11:06:45 +0530

    Modified code and test cases

--- Day_3/search_in_sorted_rotated_array.cpp
+++ Day_3/search_in_sorted_rotated_array.cpp
@@ -9,47 +9,33 @@ Problem Statement: Given a sorted and rotated array nums, find the index of a va
 #include<bits/stdc++.h>
 using namespace std;
 
-int findPeak(vector<int> &nums, int low, int high){
-	int mid;
-	if(high<low)
-		return -1;
-	if(high==low)
-		return low;
-	mid = low + (high-low)/2;
-
-	if(mid<high && nums[mid]>nums[mid+1])
-		return mid;
-	if(low<mid && nums[mid-1]>nums[mid])
-		return mid-1;
-	if(nums[low]>=nums[mid])
-		return findPeak(nums,low,mid-1);
-	return findPeak(nums,mid+1,high);
-}
-
-int binarySearch(vector<int> &nums, int start, int end,int val){
-	int mid;
-	if(end<start)
-		return -1;
-	mid = start + (end-start)/2;
-	if(nums[mid]==val)
-		return mid;
-	else if(nums[mid]>val)
-		return binarySearch(nums,start,mid-1,val);
-	else
-		return binarySearch(nums,mid+1,end,val);
-}
-
 int searchSortedRotated(vector<int> &nums, int n, int val){
-    int peak = findPeak(nums, 0, n-1);
-    if(peak==-1){
-	    return binarySearch(nums,0,n-1,val);
-    }
-    if(nums[peak]==val)
-	    return peak;
-    if(nums[0]<=val)
-	    return binarySearch(nums, 0, peak-1,val);
-    return binarySearch(nums,peak,n-1,val);
+    int low = 0, high = n-1,mid, res = -1;
 
+	while(low<=high){
+		mid = low + (high-low)/2;
+		if(nums[mid]==val)
+			res = mid;
+		if(nums[mid]>nums[high]){
+			if(val<nums[mid] && val>=nums[low])
+				high = mid-1;
+			else
+				low = mid+1;
+		}
+		else if(nums[low]>nums[mid]){
+			if(val>nums[mid] && val<=nums[high])
+				low = mid+1;
+			else
+				high = mid-1;
+		}
+		else{
+			if(nums[mid]>val)
+				high = mid-1;
+			else
+				low = mid+1;
+		}
+	}
+	return res;
 }
 
 
